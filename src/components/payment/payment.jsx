a639abba1dc0e28bcfb8536b88529ae01b2b76e7
AL-Hourani <jaafar.alhorani.02@gmail.com>
2025-05-28 03:45:07 -0700

    my first commit in a branch payment

---
+++ src/components/payment/payment.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+import React from 'react'
+
+const payment = () => {
+  return (
+    <div>
+        <h1>payment page</h1>
+        <p>im working now in a payment branch</p>
+
+        {/* some code */}
+        <input type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default payment
